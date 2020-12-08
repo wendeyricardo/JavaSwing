efd7365d707e15bb67d0dcfff06641bae32e95db
wendey <wendeyricardo@gmail.com>
2020-12-08 00:27:52 -0300

    criando a conexão com o bd e os getters e setters da classe cliente

---
+++ src/br/com/projeto/jdbc/ConnectionFactory.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Wendey-HD
+ */
+public class ConnectionFactory {
+    
+    public Connection getConnection(){
+        
+        try {
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1/bdvendas","usuariocurso","123");
+        } catch (Exception erro) {
+            throw new RuntimeException(erro);
+        }
+    }
+    
+}
